a2ef88dbaf1d0d50140957ee4166155296d0a735
Joana Teixeira <joanajteixeira@outlook.com>
2021-01-21 15:16:49 +0000

    Updates

---
+++ precache-manifest.f1b10b3fd73a6f388f8fd457880740c1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f832553c7ba33eb88ad276e8fc216866",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "03c6f7a83b0e4413cd54",
+    "url": "/website/static/css/2.e712defa.chunk.css"
+  },
+  {
+    "revision": "93dad7c71afab7b583f2",
+    "url": "/website/static/css/main.5a6634b5.chunk.css"
+  },
+  {
+    "revision": "03c6f7a83b0e4413cd54",
+    "url": "/website/static/js/2.a1863879.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/website/static/js/2.a1863879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93dad7c71afab7b583f2",
+    "url": "/website/static/js/main.1b37137e.chunk.js"
+  },
+  {
+    "revision": "7eb5a83b35c0738c174a",
+    "url": "/website/static/js/runtime-main.2b603411.js"
+  },
+  {
+    "revision": "3b70944f8357571925eeb67295c90597",
+    "url": "/website/static/media/chai.3b70944f.png"
+  },
+  {
+    "revision": "ae00d7e4823dbf150c694174f7bfb7f0",
+    "url": "/website/static/media/express.ae00d7e4.jpg"
+  },
+  {
+    "revision": "19603ea2fc1a4de6f78641f31749ed2b",
+    "url": "/website/static/media/mongoose.19603ea2.jpg"
+  },
+  {
+    "revision": "418e52e2c7a77c587e9885624a2e40fd",
+    "url": "/website/static/media/react-native.418e52e2.png"
+  },
+  {
+    "revision": "f19358ed752c33152326ec87044964ef",
+    "url": "/website/static/media/werkzeug.f19358ed.png"
+  }
+]);
